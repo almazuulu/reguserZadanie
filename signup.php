2b99af7439a0f7d3efef4becacb5b7138ae0d879
almazuulu <almazuulu@gmail.com>
2019-08-03 11:52:35 +0600

    начало работы над страницей регистрации

--- signup.php
+++ signup.php
@@ -1,3 +1,23 @@
 <?php
 	include("includes/header.php"); 
 ?>
+<title>Регистрация нового пользователя</title>
+</head>
+<body>
+	<div class="container">
+		<div class="login-form col-md-4 offset-md-4">
+			<div class="jumbotron" style="margin-top: 20px; padding-top: 25px;">
+				<h3 align="center"> Форма Регистрации</h3>
+				<form method="post" enctype="multipart/form-data">	
+					<div class="form-group">
+					<label>Имя пользователя : </label>
+					<input type="text" name="firstName" placeholder="Имя" class="form-control">
+				</div>
+			</div>
+			
+		</div>		
+			
+	</div>		
+
+</body>
+</html>
