0ca0905defe597f0f31914342ea31f399febafa6
almazuulu <almazuulu@gmail.com>
2019-08-04 00:44:52 +0600

    минорные исправления

--- loginpage.php
+++ loginpage.php
@@ -1,4 +1,5 @@
 <?php
+	//подключение необходимых файлов
 	include("includes/header.php"); 
 ?>
 <title>Страница Авторизации </title>
@@ -9,18 +10,24 @@
   		<div class="jumbotron" style="margin-top: 50px; padding-top: 20px;">
   			<h2 align="center" style="color: green;"> Форма Авторизации </h2>
   			<form method="post">
-
   				<!--Ввод email-->
   				<div class="form-group">
-  					<label>Email :</label>
+  					<label>E-mail :</label>
   					<input type="email" name="emailUser" class="form-control"/>
   				</div>
 
   				<!--Ввод пароля-->
   				<div class="form-group">
-  					<label>Email :</label>
-  					<input type="email" name="emailUser" class="form-control"/>
+  					<label>Пароль :</label>
+  					<input type="password" name="passwrd" class="form-control"/>
   				</div>
+
+  				<!--Запомнить пользователя-->
+  				<div class="form-group">
+  					<input type="checkbox" name="check"/>
+  					Запомнить меня!
+  				</div>
+  			</form>
   			
   		</div>
   		
