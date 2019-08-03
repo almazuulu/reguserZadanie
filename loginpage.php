8b423e4054442ed2713bf54ef786be2e56bc346a
almazuulu <almazuulu@gmail.com>
2019-08-04 00:37:48 +0600

    создание страницы авторизации и начало работы

---
+++ loginpage.php
@@ -0,0 +1,32 @@
+<?php
+	include("includes/header.php"); 
+?>
+<title>Страница Авторизации </title>
+</head>
+<body>
+  <div class="container">
+  	<div class="login-form col-md-4 offset-md-4">
+  		<div class="jumbotron" style="margin-top: 50px; padding-top: 20px;">
+  			<h2 align="center" style="color: green;"> Форма Авторизации </h2>
+  			<form method="post">
+
+  				<!--Ввод email-->
+  				<div class="form-group">
+  					<label>Email :</label>
+  					<input type="email" name="emailUser" class="form-control"/>
+  				</div>
+
+  				<!--Ввод пароля-->
+  				<div class="form-group">
+  					<label>Email :</label>
+  					<input type="email" name="emailUser" class="form-control"/>
+  				</div>
+  			
+  		</div>
+  		
+  	</div>
+  	
+  </div>
+
+</body>
+</html>
