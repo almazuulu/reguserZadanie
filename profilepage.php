77b552e233feee60a39bd08776a4126700d9d0d1
almazuulu <almazuulu@gmail.com>
2019-08-04 12:33:01 +0600

    отображение информаций с базы данных и начало процедуры выхода

--- profilepage.php
+++ profilepage.php
@@ -4,7 +4,13 @@
 	include("includes/config.php");
 	session_start();
 	$email=$_SESSION['mailUser'];
- echo "Вы только что зашли на сайт!";
+	mysqli_query($conDB,"SET NAMES utf8"); //Устанавливаем кирилицу, чтобы не было проблем с отображением
+	$result= mysqli_query($conDB,"SELECT firstName,lastName FROM users WHERE mail='$email'");
+	$showing= mysqli_fetch_array($result);
+	//print_r($showing); //Для проверки
+	$fName= $showing['firstName'];
+	$lName=$showing['lastName'];
+    //echo "Вы только что зашли на сайт!";
 ?>
 <title>Профиль страницы</title>
 <style type="text/css">
@@ -19,7 +25,10 @@
 <body id="body-bg">
 	<div class="container" style="background-color: #fff; margin-top: 20px; margin-bottom: 20px;width: 1200px;height: 600px;">
 		<?php echo $email;  ?>
-		<button class="btn btn-outline-success" style="float:right;margin-top: 20px;">Выйти</button>
+
+		<h2 align="center">Добро пожаловать <?php echo $fName." ".$lName ?> !</h2>
+		<a href="exit.php"><button class="btn btn-outline-success" style="float:right;margin-top: 20px;">Выйти</button></a>
+	</br></br>
 	</div>
 
 </body>
